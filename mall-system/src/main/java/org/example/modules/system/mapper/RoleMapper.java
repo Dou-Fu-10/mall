247b30c5b022b971cbb78c3b1129799fffbc30d4
2980059119 <2134059281@qq.com>
2023-07-13 14:04:22 +0800

    修改命名规则添加商品

--- mall-system/src/main/java/org/example/modules/system/mapper/RoleMapper.java
+++ mall-system/src/main/java/org/example/modules/system/mapper/RoleMapper.java
@@ -20,10 +20,10 @@ public interface RoleMapper extends BaseMapper<RoleEntity> {
 
 
     /**
-     * 首先，在ums_users_roles和ums_role表之间进行内连接（INNER JOIN）。连接条件是ums_users_roles表的role_id与ums_role表的id匹配，并且ums_role表的delete_flag字段为0且enabled字段为1。
-     * 然后，在ums_role表和ums_roles_menus表之间进行内连接。连接条件是ums_role表的id与ums_roles_menus表的role_id匹配。
-     * 最后，在ums_roles_menus表和ums_menu表之间进行内连接。连接条件是ums_roles_menus表的menu_id与ums_menu表的id匹配，并且ums_menu表的delete_flag字段为0。
-     * 最终，使用WHERE子句筛选出ums_users_roles表中user_id为1的记录。
+     * 首先，在ums_admins_roles_relation和ums_role表之间进行内连接（INNER JOIN）。连接条件是ums_admins_roles_relation表的role_id与ums_role表的id匹配，并且ums_role表的delete_flag字段为0且enabled字段为1。
+     * 然后，在ums_role表和ums_roles_menus_relation表之间进行内连接。连接条件是ums_role表的id与ums_roles_menus_relation表的role_id匹配。
+     * 最后，在ums_roles_menus_relation表和ums_menu表之间进行内连接。连接条件是ums_roles_menus_relation表的menu_id与ums_menu表的id匹配，并且ums_menu表的delete_flag字段为0。
+     * 最终，使用WHERE子句筛选出ums_admins_roles_relation表中user_id为1的记录。
      *
      * @param id 用户id
      * @return 根据用户Id查找权限
@@ -31,17 +31,17 @@ public interface RoleMapper extends BaseMapper<RoleEntity> {
     @Select("SELECT" +
             "  menu.permission" +
             " FROM " +
-            "  ums_users_roles uur" +
+            "  ums_admins_roles_relation uarr" +
             "  INNER JOIN ums_role ur" +
-            "    ON uur.role_id = ur.id" +
+            "    ON uarr.role_id = ur.id" +
             "    AND ur.delete_flag = 0" +
             "    AND ur.enabled = 1" +
-            "  INNER JOIN ums_roles_menus urm" +
-            "    ON ur.id = urm.role_id" +
+            "  INNER JOIN ums_roles_menus_relation urmr" +
+            "    ON ur.id = urmr.role_id" +
             "  INNER JOIN ums_menu menu" +
-            "    ON urm.menu_id = menu.id" +
+            "    ON urmr.menu_id = menu.id" +
             "    AND menu.delete_flag = 0" +
-            " WHERE uur.user_id = #{userId}")
-    Set<String> findPermissionByUserId(@Param("userId") Long id);
+            " WHERE uarr.admin_id = #{adminId}")
+    Set<String> findPermissionByUserId(@Param("adminId") Long id);
 }
 
