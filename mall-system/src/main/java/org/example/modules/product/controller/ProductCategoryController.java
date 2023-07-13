72fa0f8dea3ae76e4332db3f4fb933d51ad3c337
2980059119 <2134059281@qq.com>
2023-07-13 22:08:55 +0800

    商品分类

--- mall-system/src/main/java/org/example/modules/product/controller/ProductCategoryController.java
+++ mall-system/src/main/java/org/example/modules/product/controller/ProductCategoryController.java
@@ -2,13 +2,13 @@ package org.example.modules.product.controller;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
 import org.example.modules.product.entity.ProductCategoryEntity;
 import org.example.modules.product.service.ProductCategoryService;
-import jakarta.annotation.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.Serializable;
--- mall-system/src/main/java/org/example/modules/product/service/impl/ProductCategoryServiceImpl.java
+++ mall-system/src/main/java/org/example/modules/product/service/impl/ProductCategoryServiceImpl.java
@@ -1,10 +1,10 @@
 package org.example.modules.product.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.springframework.stereotype.Service;
 import org.example.modules.product.entity.ProductCategoryEntity;
 import org.example.modules.product.mapper.ProductCategoryMapper;
 import org.example.modules.product.service.ProductCategoryService;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by PanShiFu 2023-07-13 22:08:15
