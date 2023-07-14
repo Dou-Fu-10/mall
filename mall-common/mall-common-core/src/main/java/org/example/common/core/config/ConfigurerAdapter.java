a6730bf1326c15cf945fad8fa4a7ff085dea162f
2980059119 <2134059281@qq.com>
2023-07-14 14:32:37 +0800

    商品分类和商品属性、商品

--- mall-common/mall-common-core/src/main/java/org/example/common/core/config/ConfigurerAdapter.java
+++ mall-common/mall-common-core/src/main/java/org/example/common/core/config/ConfigurerAdapter.java
@@ -1,90 +1,90 @@
-//package org.example.common.core.config;
+package org.example.common.core.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimeZone;
+
+/**
+ * Created by PanShiFu 2023/7/14
+ *
+ * @author PanShiFu
+ * @date 2023/7/14
+ * @Description WebMvcConfigurer
+ */
+@Configuration
+@EnableWebMvc
+public class ConfigurerAdapter implements WebMvcConfigurer {
+
+    /**
+     * DateTime格式化字符串
+     */
+    private static final String DEFAULT_DATETIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    /**
+     * Date格式化字符串
+     */
+    private static final String DEFAULT_DATE_PATTERN = "yyyy-MM-dd";
+    /**
+     * Time格式化字符串
+     */
+    private static final String DEFAULT_TIME_PATTERN = "HH:mm:ss";
+    /**
+     * 文件配置
+     */
+//    private final FileProperties properties;
 //
-//import com.fasterxml.jackson.annotation.JsonInclude;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.fasterxml.jackson.databind.SerializationFeature;
-//import com.fasterxml.jackson.databind.module.SimpleModule;
-//import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
-//import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-//import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-//import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-//import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
-//import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-//import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-//import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.converter.HttpMessageConverter;
-//import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//import org.springframework.web.filter.CorsFilter;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//import java.math.BigDecimal;
-//import java.math.BigInteger;
-//import java.text.SimpleDateFormat;
-//import java.time.LocalDate;
-//import java.time.LocalDateTime;
-//import java.time.LocalTime;
-//import java.time.format.DateTimeFormatter;
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.TimeZone;
-//
-///**
-// * Created by PanShiFu 2023/7/14
-// *
-// * @author PanShiFu
-// * @date 2023/7/14
-// * @Description WebMvcConfigurer
-// */
-//@Configuration
-//@EnableWebMvc
-//public class ConfigurerAdapter implements WebMvcConfigurer {
-//
-//    /**
-//     * DateTime格式化字符串
-//     */
-//    private static final String DEFAULT_DATETIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
-//    /**
-//     * Date格式化字符串
-//     */
-//    private static final String DEFAULT_DATE_PATTERN = "yyyy-MM-dd";
-//    /**
-//     * Time格式化字符串
-//     */
-//    private static final String DEFAULT_TIME_PATTERN = "HH:mm:ss";
-//    /**
-//     * 文件配置
-//     */
-////    private final FileProperties properties;
-////
-////    public ConfigurerAdapter(FileProperties properties) {
-////        this.properties = properties;
-////    }
-//
-//    @Bean
-//    public CorsFilter corsFilter() {
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowCredentials(true);
-//        // 允许访问的客户端域名
-//        List<String> allowedOriginPatterns = new ArrayList<>();
-//        allowedOriginPatterns.add("*");
-//        config.setAllowedOriginPatterns(allowedOriginPatterns);
-//        config.addAllowedHeader("*");
-//        config.addAllowedMethod("*");
-//        source.registerCorsConfiguration("/**", config);
-//        return new CorsFilter(source);
+//    public ConfigurerAdapter(FileProperties properties) {
+//        this.properties = properties;
 //    }
-//
-//    /**
-//     * 添加自定义转换器
-//     *
-//     * @param converters 转换器
-//     */
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // 允许访问的客户端域名
+        List<String> allowedOriginPatterns = new ArrayList<>();
+        allowedOriginPatterns.add("*");
+        config.setAllowedOriginPatterns(allowedOriginPatterns);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+    /**
+     * 添加自定义转换器
+     *
+     * @param converters 转换器
+     */
 //    @Override
 //    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
 //        MappingJackson2HttpMessageConverter jackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
@@ -136,4 +136,4 @@
 //        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
 //        return objectMapper;
 //    }
-//}
+}
